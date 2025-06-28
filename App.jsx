edd77b80e89f3c0ee7266b86a41a0d7229e6a097
Ahmed Bilal <ahmedbilalmobile1@gmail.com>
2025-06-28 17:38:09 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Components from './Components/components'; // Your main/home component
+import Components from './Components/components'; // main/home component
 import Contact from './Components/Contact'; 
 import TermsPrivacy from './Components/TermsPrivacy'; 
 import Admin from './Components/admin';
+import ScrollToTop from './Components/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
@@ -13,7 +14,7 @@ const Layout = () => {
     <div>
       <Header />
      
-      <div style={{ padding: '20px' }}>
+      <div style={{ padding: '' }}>
         <Outlet /> {/* This renders the content for each route */}
       </div>
       <Footer />
@@ -23,11 +24,10 @@ const Layout = () => {
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
     <Routes>
-      {/* Wrap all routes in the Layout component */}
       <Route path="/" element={<Layout />}>
-        {/* Define individual routes that render within the Layout */}
-        <Route index element={<Components />} /> {/* Default route */}
+        <Route index element={<Components />} />
         <Route path="contact" element={<Contact />} />
         <Route path="terms-privacy" element={<TermsPrivacy />} />
         <Route path="/admin" element={<Admin />} />
